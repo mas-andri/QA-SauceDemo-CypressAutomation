81f70dfd1e9b8f43045e23959c9a07382a71742c
Margono Andri Hastiko <mgoandri.has@gmail.com>
2025-09-21 14:57:51 +0700

    add cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
 const { defineConfig } = require("cypress");
+// const {
+//   beforeRunHook,
+//   afterRunHook,
+// } = require("cypress-mochawesome-reporter/lib");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+
+      // on("before:run", async (details) => {
+      //   console.log("override before:run");
+      //   await beforeRunHook(details);
+      // });
+
+      // on("after:run", async () => {
+      //   console.log("override after:run");
+      //   await afterRunHook();
+      // });
     },
     specPattern: "cypress/e2e/*.js",
   },
